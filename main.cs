75216f5a85228711bab0df45a22140b71bf6a10c
Gilledone <migillis89@gmail.com>
2020-07-26 04:47:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Select a color");
+    var color = Console.ReadLine();
+
+    if (color == "red" || color == "pink" || color == "yellow" || color == "orange" || color == "gold"){
+      Console.WriteLine("The color " + color + " is warm");
+    }
+    else if (color == "blue" || color == "purple" || color == "magenta" || color == "green"){
+      Console.WriteLine("The color " + color + " is cool");
+    }
+    else if (color == "black" || color == "white" || color == "ivory" || color == "brown" || color == "beige"){
+      Console.WriteLine("The color " + color + " is neutral");
+    }
+    else{
+      Console.WriteLine("Unable to determine the color temperature for the color " + color);
+    }
+  }
+}
